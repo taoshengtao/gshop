f62cd97da5b061f4dbd4969cd6c036fb671b7198
liyi <liyi@hjeasygo.com>
2020-05-08 17:18:49 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router// 使用上vue-router
+  // store // 使用上vuex
+})
